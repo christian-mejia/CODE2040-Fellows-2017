092093b1919c1d9fc5c392e702b6736cb858d382
christian-mejia <cm4080@nyu.edu>
2016-10-27 22:38:33 -0400

    Add files via upload

---
+++ Step 2.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+#token = {'token': '656e703aa01b60f64f4c5b70501219f6'}
+
+string = requests.post("http://challenge.code2040.org/api/reverse", data={'token': '656e703aa01b60f64f4c5b70501219f6'})
+
+reversedStr = string.text[::-1]
+
+data = requests.post("http://challenge.code2040.org/api/reverse/validate", data = {'token': '656e703aa01b60f64f4c5b70501219f6', 'string':reversedStr})
