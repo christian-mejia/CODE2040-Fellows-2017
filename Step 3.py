74f49d04771f0a57df9d4bc1efc0d7ba6d48b496
christian-mejia <cm4080@nyu.edu>
2016-10-31 00:26:52 -0400

    Update Step 3.py

--- Step 3.py
+++ Step 3.py
@@ -1,13 +1,20 @@
+#Needle in a haystack
+
 import json
 import requests
 
-needleInStack = requests.post("http://challenge.code2040.org/api/haystack", data={'token':'656e703aa01b60f64f4c5b70501219f6'})
+challengeRequest = requests.post("http://challenge.code2040.org/api/haystack", 
+                              data={'token':'656e703aa01b60f64f4c5b70501219f6'})
 
-needleInStack = needleInStack.json()
+needleInStack = challengeRequest.json()
 
+#Retrieving values from dictionary
 needle = needleInStack['needle']
 haystack = needleInStack['haystack']
 
+#Looping through haystack to find needle
 for x in range(0,len(needleInStack)):
     if(haystack[x] == needle):
-        r = requests.post("http://challenge.code2040.org/api/haystack/validate", data={'token':'656e703aa01b60f64f4c5b70501219f6', 'needle': x})
+        r = requests.post("http://challenge.code2040.org/api/haystack/validate", 
+                          data={'token':'656e703aa01b60f64f4c5b70501219f6', 
+                                'needle': x})
