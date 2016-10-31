1d756da9c9ae22f621f6da6238a0d5dd2ce8a600
christian-mejia <cm4080@nyu.edu>
2016-10-31 00:20:27 -0400

    Update Step 1.py

--- Step 1.py
+++ Step 1.py
@@ -7,4 +7,3 @@ payload = {'token': '656e703aa01b60f64f4c5b70501219f6',
            'github': 'https://github.com/christian-mejia/CODE2040-Fellows-2017'}
 
 r = requests.post("http://challenge.code2040.org/api/register", data=payload)
-
