7dd42c5ea25f815a4c7002de12c582396fd23a14
christian-mejia <cm4080@nyu.edu>
2016-10-31 00:30:12 -0400

    Update Step 2.py

--- Step 2.py
+++ Step 2.py
@@ -8,6 +8,6 @@ string = requests.post("http://challenge.code2040.org/api/reverse",
 
 reversedStr = string.text[::-1]   #Reverses string
 
-data = requests.post("http://challenge.code2040.org/api/reverse/validate", 
+r = requests.post("http://challenge.code2040.org/api/reverse/validate", 
                      data = {'token': '656e703aa01b60f64f4c5b70501219f6', 
                              'string':reversedStr})
